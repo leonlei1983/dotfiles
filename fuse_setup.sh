cf8095938f00320e58948445e948cda682f0561d
David Hu <chin33z@gmail.com>
2015-03-09 17:50:51 +0800

    Update fuse_setup.sh

--- fuse_setup.sh
+++ fuse_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-watchman watch-del ~/bs-build-part
+#watchman watch-del ~/bs-build-part
 fusermount -u ~/mergeBS
 rm -fr ~/bs-build-part
 cd ~
@@ -8,7 +8,8 @@ cd ~
 git clone git@192.168.81.7:bs-build-part/bs-build-part.git
 mkdir -p ~/mergeBS
 unionfs-fuse -o cow /root/bs-build-part=RW:/root/NewRepo=RO /root/mergeBS
-watchman watch ~/bs-build-part
+#watchman watch ~/bs-build-part
 
-INIT_TIME=$(watchman clock /root/bs-build-part/ | jq -M .clock)
-sed -i 's/"n:tmp"/'${INIT_TIME}'/g' ~/dotfiles/bs-build-part.json
+# get the current time
+#INIT_TIME=$(watchman clock /root/bs-build-part/ | jq -M .clock)
+#sed -i 's/"n:tmp"/'${INIT_TIME}'/g' ~/dotfiles/bs-build-part.json
