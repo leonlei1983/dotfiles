46c5c51e4ba8c637e29b152ef539bc6b1e64b4e5
David Hu <chin33z@gmail.com>
2015-04-01 13:34:54 +0800

    Update fuse_setup.sh

--- fuse_setup.sh
+++ fuse_setup.sh
@@ -3,7 +3,7 @@
 watchman shutdown-server
 echo 100000 > /proc/sys/fs/inotify/max_user_watches
 
-sed -i 's/"c[^"]*"/"init_time"/g' ~/dotfiles/bs-build-part.json
+sed -i 's/"c:[^"]*"/"init_time"/g' ~/dotfiles/bs-build-part.json
 fusermount -u ~/mergeBS
 rm -fr ~/bs-build-part
 cd ~
