75491008f84f50971c1830b732ef7ac41d3c3fa0
David Hu <chin33z@gmail.com>
2015-05-06 08:36:56 +0000

    use union-fs

--- fuse_setup.sh
+++ fuse_setup.sh
@@ -4,19 +4,20 @@ watchman shutdown-server
 echo 100000 > /proc/sys/fs/inotify/max_user_watches
 
 sed -i 's/"c:[^"]*"/"init_time"/g' ~/dotfiles/bs-build-part.json
-# fusermount -u ~/mergeBS
-umount /root/mergeBS
+fusermount -u ~/mergeBS
+# umount ~/mergeBS
 rm -fr ~/bs-build-part ~/tmp
 cd ~
 
 git clone git@192.168.81.7:bs-build-part/bs-build-part.git
 mkdir -p ~/mergeBS ~/tmp
 # overlayfs cannot overlay the dir mount from outside container, so we copy the dir to local
-if [ ! -d "NewRepo2" ]; then
-    cp -r NewRepo/ NewRepo2
-fi
-# unionfs-fuse -o cow /root/bs-build-part=RW:/root/NewRepo=RO /root/mergeBS
-mount -t overlay overlay -olowerdir=/root/NewRepo2,upperdir=/root/bs-build-part,workdir=/root/tmp /root/mergeBS
+# if [ ! -d "NewRepo2" ]; then
+#     cp -r NewRepo/ NewRepo2
+# fi
+unionfs-fuse -o cow /root/bs-build-part=RW:/root/NewRepo=RO /root/mergeBS
+# lower=read-only dir , upper=rw dir
+# mount -t overlay overlay -olowerdir=/root/NewRepo2,upperdir=/root/bs-build-part,workdir=/root/tmp /root/mergeBS
 watchman watch ~/bs-build-part
 
 # get the repository created time
