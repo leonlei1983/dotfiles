437334f12e96063886462b9023a0b851615960b4
David Hu <chin33z@gmail.com>
2015-05-11 10:22:51 +0800

    Update fuse_setup.sh

--- fuse_setup.sh
+++ fuse_setup.sh
@@ -11,9 +11,9 @@ cd ~
 git clone git@192.168.81.7:bs-build-part/bs-build-part.git
 mkdir -p ~/mergeBS ~/tmp
 # lower=read-only dir , upper=rw dir
-mount -t overlay overlay -olowerdir=/root/NewRepo,upperdir=/root/bs-build-part,workdir=/root/tmp /root/mergeBS
+mount -t overlay overlay -olowerdir=/root/NewRepo,upperdir=${HOME}/bs-build-part,workdir=${HOME}/tmp ${HOME}/mergeBS
 watchman watch ~/bs-build-part
 
 # get the repository created time
-INIT_TIME=$(watchman clock /root/bs-build-part/ | jq -M .clock)
+INIT_TIME=$(watchman clock ${HOME}/bs-build-part/ | jq -M .clock)
 sed -i 's/"init_time"/'${INIT_TIME}'/g' ~/dotfiles/bs-build-part.json
