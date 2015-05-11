b61287b51b27faa1a72fe1bd752ad55e77a201ab
David Hu <chin33z@gmail.com>
2015-05-11 09:09:38 +0000

    fix for normal user

--- fuse_setup.sh
+++ fuse_setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 watchman shutdown-server
-echo 100000 > /proc/sys/fs/inotify/max_user_watches
+#echo 100000 > /proc/sys/fs/inotify/max_user_watches
 
 sed -i 's/"c:[^"]*"/"init_time"/g' ~/dotfiles/bs-build-part.json
-umount ~/mergeBS
-rm -fr ~/bs-build-part ~/tmp
+sudo umount ~/mergeBS
+sudo rm -fr ~/bs-build-part ~/tmp
 cd ~
 
+sudo chown ${USER} .ssh
 git clone git@192.168.81.7:bs-build-part/bs-build-part.git
 mkdir -p ~/mergeBS ~/tmp
 # lower=read-only dir , upper=rw dir
-mount -t overlay overlay -olowerdir=/root/NewRepo,upperdir=${HOME}/bs-build-part,workdir=${HOME}/tmp ${HOME}/mergeBS
+sudo mount -t overlay overlay -olowerdir=/root/NewRepo,upperdir=${HOME}/bs-build-part,workdir=${HOME}/tmp ${HOME}/mergeBS
 watchman watch ~/bs-build-part
 
 # get the repository created time
