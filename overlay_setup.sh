d11726641b6fecd33068853c9b90e550a1acf395
David Hu <chin33z@gmail.com>
2015-05-13 12:27:20 +0800

    Update overlay_setup.sh

--- overlay_setup.sh
+++ overlay_setup.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# watchman shutdown-server
-# echo 100000 > /proc/sys/fs/inotify/max_user_watches
-
-# sed -i 's/"c:[^"]*"/"init_time"/g' ~/dotfiles/bs-build-part.json
-
 umount ~/nasx86_4-2-0-x86_64_bs
 rm -fr ~/tmp ~/nasx86_4-2-0-x86_64_bs
 cd ~
 
-#sudo chown ${USER} .ssh
 mkdir -p ~/tmp
 git clone git@192.168.81.7:nasx86_4-2-0-x86_64_bs/nasx86_4-2-0-x86_64_bs.git
 # lower=read-only dir , upper=rw dir
 mount -t overlay overlay -olowerdir=/root/NasX86_4.2.0,upperdir=/root/nasx86_4-2-0-x86_64_bs,workdir=${HOME}/tmp /root/nasx86_4-2-0-x86_64_bs
-# watchman watch ~/bs-build-part
-
-# get the repository created time
-# INIT_TIME=$(watchman clock ${HOME}/bs-build-part/ | jq -M .clock)
-# sed -i 's/"init_time"/'${INIT_TIME}'/g' ~/dotfiles/bs-build-part.json
