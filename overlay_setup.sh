20a83b523235be8f9c7096734dba4e02647a81ce
David Hu <chin33z@gmail.com>
2015-05-28 06:59:28 +0000

    add /mnt/pub NFS mount

--- overlay_setup.sh
+++ overlay_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 umount ~/nasx86_4-2-0-x86_64_bs
-rm -fr ~/tmp ~/nasx86_4-2-0-x86_64_bs
+umount /mnt/pub
+rm -fr ~/tmp ~/nasx86_4-2-0-x86_64_bs /mnt/pub
 cd ~
 
-mkdir -p ~/tmp
+mkdir -p ~/tmp /mnt/pub
 git clone git@192.168.81.7:nasx86_4-2-0-x86_64_bs/nasx86_4-2-0-x86_64_bs.git
 # lower=read-only dir , upper=rw dir
 mount -t overlay overlay -olowerdir=/root/NasX86_4.2.0,upperdir=/root/nasx86_4-2-0-x86_64_bs,workdir=${HOME}/tmp /root/nasx86_4-2-0-x86_64_bs
+mount.nfs -o nolock 172.17.21.5:/pub /mnt/pub
