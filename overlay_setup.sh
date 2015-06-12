087fb88285b75b2daebf8297cb63e472b6d89f91
David Hu <chin33z@gmail.com>
2015-06-12 13:44:19 +0800

    Update overlay_setup.sh

--- overlay_setup.sh
+++ overlay_setup.sh
@@ -9,5 +9,5 @@ mkdir -p ~/tmp /mnt/pub /opt/cross-project/x86/sys-root/bin
 ln -sf /usr/bin/openssl /opt/cross-project/x86/sys-root/bin/openssl
 git clone git@192.168.81.7:nasx86_4-2-0-x86_64_bs/nasx86_4-2-0-x86_64_bs.git
 # lower=read-only dir , upper=rw dir
-mount -t overlay overlay -olowerdir=/root/NasX86_4.2.0,upperdir=/root/nasx86_4-2-0-x86_64_bs,workdir=${HOME}/tmp /root/nasx86_4-2-0-x86_64_bs
+mount -t overlay overlay -olowerdir=/root/NasX86,upperdir=/root/nasx86_4-2-0-x86_64_bs,workdir=${HOME}/tmp /root/nasx86_4-2-0-x86_64_bs
 mount.nfs -o nolock 172.17.21.5:/pub /mnt/pub
