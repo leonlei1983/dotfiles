80dcbcf196fa063f465fe8f12e73c72c87fcbaf5
Yi Hu <yihu@qnap.com>
2015-07-03 10:46:44 +0800

    add .bash_profile symlink to .bashrc

--- link.sh
+++ link.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+######################################################
+# This script creates symlinks from the 
+# home directory to any desired dotfiles in ~/dotfiles
+#####################################################
 
-dir=dotfiles                    # dotfiles directory
-files="screenrc bashrc git-completion"    # list of files/folders to symlink in homedir
+# dotfiles directory
+dir=dotfiles
 
-##########
+# list of files/folders to symlink in homedir
+files="screenrc bashrc git-completion"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Creating symlink to $file in container's home directory."
     ln -sf $dir/$file .$file
 done
+
+# for screen openning shell
+ln -sf .bashrc .bash_profile
